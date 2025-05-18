24a3164ecc31a89a5e1abffab1d60c86d8055743
Irina <ItsIrina_@outlook.com>
2025-05-18 14:55:42 +0800

    /

---
+++ src/main/java/cn/irina/thepitaddon/utils/DynamicInvoke.kt
@@ -0,0 +1,10 @@
+package cn.irina.thepitaddon.utils
+
+/*
+ * @Author Irina
+ * @Date 2025/5/5 15:25
+ */
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+annotation class DynamicInvoke()
---
+++ src/main/java/cn/irina/thepitaddon/utils/HideAccess.kt
@@ -0,0 +1,9 @@
+package cn.irina.thepitaddon.utils
+
+/*
+ * @Author Irina
+ * @Date 2025/5/5 15:32
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+annotation class HideAccess
