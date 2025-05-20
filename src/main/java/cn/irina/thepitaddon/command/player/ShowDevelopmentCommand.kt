c3a763a818b32d2fc11ae08e46f9b9561aa4922d
Irina <ItsIrina@outlook.com>
2025-05-20 18:28:11 +0800

    /

--- src/main/java/cn/irina/thepitaddon/command/player/ShowDevelopmentCommand.kt
+++ src/main/java/cn/irina/thepitaddon/command/player/ShowDevelopmentCommand.kt
@@ -1,17 +1,17 @@
 package cn.irina.thepitaddon.command.player
 
-import net.mizukilab.pit.util.chat.CC
 import dev.rollczi.litecommands.annotations.command.Command
 import dev.rollczi.litecommands.annotations.context.Context
 import dev.rollczi.litecommands.annotations.execute.Execute
+import net.mizukilab.pit.util.chat.CC
 import org.bukkit.command.CommandSender
 
-@Command(name = "thepit")
+@Command(name = "thepitaddon")
 class ShowDevelopmentCommand {
 
     @Execute
     fun development(@Context sender: CommandSender) {
-        sender.sendMessage(CC.translate("&bIrina&fThePitAddon"))
-        sender.sendMessage(CC.translate("&aPower By _Ir1na_, ShanguanLinG."))
+        sender.sendMessage(CC.translate("&7ThePitAddon For &cThePitUltimate"))
+        sender.sendMessage(CC.translate("&7Powered By &e_Ir1na_&7, &eShanguanLinG"))
     }
 }
