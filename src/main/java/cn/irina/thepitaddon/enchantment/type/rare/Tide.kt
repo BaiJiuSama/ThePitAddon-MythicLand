0879f6d851e307af730eafdb42d15eaa75d51cb8
Irina <ItsIrina_@outlook.com>
2025-06-06 23:56:21 +0800

    /

---
+++ src/main/java/cn/irina/thepitaddon/enchantment/type/rare/Tide.kt
@@ -0,0 +1,50 @@
+package cn.irina.thepitaddon.enchantment.type.rare
+
+import com.google.common.util.concurrent.AtomicDouble
+import net.mizukilab.pit.enchantment.AbstractEnchantment
+import net.mizukilab.pit.enchantment.rarity.EnchantmentRarity
+import net.mizukilab.pit.parm.listener.IPlayerKilledEntity
+import net.mizukilab.pit.util.cooldown.Cooldown
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
+
+/*
+ * @Author Irina
+ * @Date 2025/6/6 23:55
+ */
+
+class Tide: AbstractEnchantment(), IPlayerKilledEntity {
+    override fun getEnchantName(): String {
+        return "战酬"
+    }
+
+    override fun getMaxEnchantLevel(): Int {
+        return 3
+    }
+
+    override fun getNbtName(): String {
+        return "war_pay"
+    }
+
+    override fun getRarity(): EnchantmentRarity {
+        return EnchantmentRarity.RARE
+    }
+
+    override fun getCooldown(): Cooldown? {
+        return null
+    }
+
+    override fun getUsefulnessLore(level: Int): String {
+        return "击杀获得的经验 &b+${30 + (level * 20)}%"
+    }
+
+    override fun handlePlayerKilled(
+        level: Int,
+        killer: Player,
+        entity: Entity,
+        coin: AtomicDouble,
+        exp: AtomicDouble,
+    ) {
+        exp.getAndAdd(0.3 + (level * 0.2))
+    }
+}
