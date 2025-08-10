6afadbdf59430a02bbb3fa8cee55832b89d85565
Irina <ItsIrina_@outlook.com>
2025-08-11 04:29:51 +0800

    依旧微调 (神秘空格使我的强迫症旋转。)

--- src/main/java/cn/irina/thepitaddon/runnable/FreeCE.kt
+++ src/main/java/cn/irina/thepitaddon/runnable/FreeCE.kt
@@ -1,4 +1,5 @@
 package cn.irina.thepitaddon.runnable
+
 import cn.charlotte.pit.data.PlayerProfile
 import cn.irina.thepitaddon.Main
 import net.mizukilab.pit.util.chat.CC
@@ -13,11 +14,13 @@ import org.bukkit.entity.Player
 class FreeCE : Runnable {
     override fun run() {
         val limit = 100
+
         Bukkit.getOnlinePlayers().toList().forEach { player ->
             try {
+                if (!player.world.name.equals("afk")) return
+
                 val profile = PlayerProfile.getRawCache(player.uniqueId)
                 val exp = profile.experience
-                if (!player.world.name.equals("afk")) return
                 if (profile.prestige >= limit && profile.level >= 120) {
                     player.sendMessage(CC.translate("&b&l经验值已满! &7您已满级, 无法继续升级!"))
                     return
@@ -26,6 +29,7 @@ class FreeCE : Runnable {
                     autoPrestige(player)
                     return
                 }
+
                 val rewardExp = getRewardExp(profile.prestige)
                 profile.experience = exp + rewardExp
                 player.sendMessage(CC.translate(message).replace("%exp%", rewardExp.toInt().toString()))
@@ -36,20 +40,17 @@ class FreeCE : Runnable {
     }
 
     private fun getRewardExp(prestige: Int): Double {
-        var booster = 0.5
-        if (prestige <= 12) {
-            booster = 0.36
-        } else if (prestige <= 20) {
-            booster = 0.30
-        } else if (prestige <= 30) {
-            booster = 0.24
-        } else if (prestige <= 50) {
-            booster = 0.22
+        val booster = when {
+            prestige <= 12 -> 0.36
+            prestige <= 20 -> 0.3
+            prestige <= 30 -> 0.24
+            prestige <= 50 -> 0.22
+            else -> 0.5
         }
+
         var needExp = 0.2
-        for (level in 1..120) {
-            needExp += LevelUtil.getLevelExpRequired(prestige, level)
-        }
+        for (level in 1..120) needExp += LevelUtil.getLevelExpRequired(prestige, level)
+
         return needExp * booster + 10000
     }
 
@@ -57,31 +58,24 @@ class FreeCE : Runnable {
         val profile = PlayerProfile.getRawCache(player.uniqueId)
         val data = profile.unlockedPerkMap["FastPass"]
         //FastPass Perk
-        if (data != null) {
-            profile.experience = LevelUtil.getLevelTotalExperience(profile.getPrestige(), 50)
-        }
+        if (data != null) profile.experience = LevelUtil.getLevelTotalExperience(profile.getPrestige(), 50)
+
         profile.coins = 0.0
         profile.experience = 0.0
         profile.grindedCoins = 0.0
         profile.setPrestige(profile.getPrestige() + 1)
         //PlayerUtil.clearPlayer(player);
         //InventoryUtil.supplyItems(player);
+
         var award = 10
-        if (profile.getPrestige() > 4) {
-            award += 10
-        }
-        if (profile.getPrestige() > 10) {
-            award += 10
-        }
-        if (profile.getPrestige() > 14) {
-            award += 10
-        }
+
+        if (profile.getPrestige() > 4) award += 10
+        if (profile.getPrestige() > 10) award += 10
+        if (profile.getPrestige() > 14) award += 10
         profile.renown += award
         CC.boardCast(
             MessageType.PRESTIGE,
-            (("&e&l精通! &7" + RankUtil.getPlayerColoredName(player.name)).toString() + " &7解锁了精通 &e" + RomanUtil.convert(
-                profile.getPrestige()
-            )).toString() + " &7,gg!"
+            "&e&l精通! &7${RankUtil.getPlayerColoredName(player.name)} &7解锁了精通 &e${RomanUtil.convert(profile.getPrestige())} &7,gg!"
         )
         player.playSound(player.location, Sound.ENDERDRAGON_GROWL, 1f, 1f)
     }
