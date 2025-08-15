5fe2cd6d865505c533e81093403ed5b54e2a9e1d
ShanguanLinG <bilibiliShanguanLinG@outlook.com>
2025-08-15 23:22:37 +0800

    完善unGem

--- src/main/java/cn/irina/thepitaddon/command/player/UnGem.kt
+++ src/main/java/cn/irina/thepitaddon/command/player/UnGem.kt
@@ -5,9 +5,13 @@ import dev.rollczi.litecommands.annotations.argument.Arg
 import dev.rollczi.litecommands.annotations.command.Command
 import dev.rollczi.litecommands.annotations.context.Context
 import dev.rollczi.litecommands.annotations.execute.Execute
+import net.mizukilab.pit.util.chat.CC
 import net.mizukilab.pit.util.item.ItemBuilder
+import net.mizukilab.pit.util.item.ItemUtil
+import org.bukkit.Material
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 /*
  * @Author Irina, ShanguanLinG
@@ -24,18 +28,42 @@ class UnGem {
         @Arg gemTypeName: String,
         @Arg player: Player
     ) {
-        var item = player.inventory.itemInHand
+        val item = player.inventory.itemInHand
+        if (item == null || item.type == Material.AIR) {
+            sender.sendMessage(CC.translate("$prefix &c请手持物品!"))
+            return
+        }
         if ("Gem" == gemTypeName) {
-            var newItem = ItemBuilder(item).changeNbt("boostedByGem", 0).build()
+            if (!isBoostedByGem(item)) {
+                sender.sendMessage(CC.translate("$prefix &c这件物品上并没有使用过 &a遵纪守法的宝石!"))
+                return
+            }
+            val newItem = ItemBuilder(item).changeNbt("boostedByGem", 0).build()
             player.inventory.itemInHand = newItem
             player.updateInventory()
+            player.sendMessage(CC.translate("$prefix &a成功移除了 &a遵纪守法的宝石 &7点缀."))
             return
         }
         if ("GlobalGem" == gemTypeName) {
-            var newItem = ItemBuilder(item).changeNbt("boostedByGlobalGem", 0).build()
+            if (!isBoostedByGlobalGem(item)) {
+                sender.sendMessage(CC.translate("$prefix &c这件物品上并没有使用过 &b举世瞩目的宝石!"))
+                return
+            }
+            val newItem = ItemBuilder(item).changeNbt("boostedByGlobalGem", 0).build()
             player.inventory.itemInHand = newItem
             player.updateInventory()
+            player.sendMessage(CC.translate("$prefix &a成功移除了 &b举世瞩目的宝石 &7点缀."))
             return
         }
+    }
+
+    private fun isBoostedByGem(item: ItemStack?): Boolean {
+        val itemStringData = ItemUtil.getItemBoolData(item, "boostedByGem")
+        return itemStringData
+    }
+
+    private fun isBoostedByGlobalGem(item: ItemStack?): Boolean {
+        val itemStringData = ItemUtil.getItemBoolData(item, "boostedByGlobalGem")
+        return itemStringData
     }
 }
