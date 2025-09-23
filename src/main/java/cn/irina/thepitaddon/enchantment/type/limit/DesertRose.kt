8fb30a35e8c9430ba4b19a75d1962f0a14a329e1
ShanguanLinG <bilibiliShanguanLinG@outlook.com>
2025-09-23 19:53:41 +0800

    修复沙漠玫瑰冷却不生效问题

--- src/main/java/cn/irina/thepitaddon/enchantment/type/limit/DesertRose.kt
+++ src/main/java/cn/irina/thepitaddon/enchantment/type/limit/DesertRose.kt
@@ -4,7 +4,9 @@ import cn.charlotte.pit.ThePit
 import cn.irina.thepitaddon.manager.PitManager
 import cn.irina.thepitaddon.utils.TimeUtil
 import com.google.common.util.concurrent.AtomicDouble
+import net.minecraft.server.v1_8_R3.BlockPosition
 import net.minecraft.server.v1_8_R3.ItemBow
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldEvent
 import net.mizukilab.pit.enchantment.AbstractEnchantment
 import net.mizukilab.pit.enchantment.IActionDisplayEnchant
 import net.mizukilab.pit.enchantment.param.item.BowOnly
@@ -38,7 +40,8 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
 
     companion object {
         private val cooldown: HashMap<UUID, Cooldown> = HashMap()
-        private val laserCooldown: HashMap<UUID, Cooldown> = HashMap()
+        private val redLaserCooldown: HashMap<UUID, Cooldown> = HashMap()
+        private val blueLaserCooldown: HashMap<UUID, Cooldown> = HashMap()
     }
 
     private val pitApi = ThePit.getApi()
@@ -65,20 +68,16 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
 
 
     override fun getUsefulnessLore(enchantLevel: Int): String {
-        val buffLevel = when (enchantLevel) {
-            1 -> "I"
-            2 -> "I"
-            3 -> "II"
-            else -> RomanUtil.convert(enchantLevel - 1)
-        }
+        val slownessBuffLevel = RomanUtil.convert(1)
+        val slownessBuffDuration = TimeUtil.formatTotalSeconds(enchantLevel * 2 + 2)
+        val weaknessBuffLevel = RomanUtil.convert(enchantLevel + 1)
+        val weaknessBuffDuration = TimeUtil.formatTotalSeconds(enchantLevel * 2 + 2)
         val speedBuffLevel = RomanUtil.convert(enchantLevel + 1)
         val speedDuration = TimeUtil.formatTotalSeconds(enchantLevel * 2 + 4)
-        val slowDuration = TimeUtil.formatTotalSeconds(enchantLevel)
-        val witherDuration = TimeUtil.formatTotalSeconds(enchantLevel)
         return "&7射箭时无需蓄力即可让箭矢以最大蓄力状态射出, 若命中目标则召唤一道激光./s" +
-                "&7激光命中敌人时, 将会产生一次爆炸并对周围敌人施加 &c缓慢 ${buffLevel} &f($slowDuration) &7与 &8凋零 ${buffLevel} &f($witherDuration)/s" +
-                "&7同时, 若爆炸接触目标生命值低于 &c${enchantLevel * 0.5 + 1.0}❤ &7时, 将直接致死. /s" +
-                "&7若射箭时处于潜行状态且激光命中目标时, 则将自身传送至爆炸处, 并获得 &b速度 ${speedBuffLevel} &f($speedDuration) &7(15秒冷却) /s"+
+                "&7激光命中敌人时, 将会产生一次爆炸并对周围敌人施加 &c虚弱 ${weaknessBuffLevel} &f($weaknessBuffDuration) &7与 &c缓慢 ${slownessBuffLevel} &f($slownessBuffDuration)/s" +
+                "&7同时, 若爆炸接触目标生命值低于 &c${enchantLevel * 0.5 + 0.5}❤ &7时, 将直接致死. /s" +
+                "&7若射箭时处于潜行状态则直接发射激光, 命中目标时, 将自身传送至爆炸处并获得 &b速度 ${speedBuffLevel} &f($speedDuration) &7(15秒冷却) /s" +
                 "&7此附魔每秒只能触发一次."
     }
 
@@ -99,9 +98,11 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
         val bow = itemStack.item as ItemBow
         Companion.cooldown[player.uniqueId] = Cooldown(1, TimeUnit.SECONDS)
         val ePlayer = (player as CraftPlayer).handle
+
         if ((event.entity as Player).isSneaking) {
-            if (!Companion.laserCooldown.getOrDefault(player.uniqueId, Cooldown(0)).hasExpired()) return
-            shootLaser(player, level, true)
+            if (!Companion.blueLaserCooldown.getOrDefault(player.uniqueId, Cooldown(0)).hasExpired()) return
+            val direction = player.location.direction.normalize()
+            shootLaser(player, level, player.eyeLocation, direction, 102f, 204f, 255f)
             event.isCancelled = true
             return
         }
@@ -109,48 +110,59 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
         event.isCancelled = true
     }
 
-    private fun shootLaser(player: Player, level: Int, isSneaking: Boolean = false) {
-        val startLocation = player.eyeLocation.clone()
-        val direction = startLocation.direction.normalize()
+    private fun shootLaser(
+        player: Player,
+        level: Int,
+        startLocation: Location,
+        direction: org.bukkit.util.Vector,
+        r: Float,
+        g: Float,
+        b: Float
+    ) {
         val laserLength = (level * 10 + 10) * 2
-        if (isSneaking) {
-            for (i in 0..laserLength * 2) {
-                val particleLocation = startLocation.clone().add(direction.clone().multiply(i * 0.5))
-                Bukkit.getOnlinePlayers()
-                    .forEach { targetPlayer -> sendRedstoneParticle(targetPlayer, particleLocation, 102f, 204f, 255f) }
-                val nearbyEntities = particleLocation.world.getNearbyEntities(particleLocation, 0.5, 0.5, 0.5)
-                for (entity in nearbyEntities) {
-                    if (entity !is Player || entity == player || entity.isDead || entity.location.y > 100) continue
-                    val directlyHitPlayer = entity as Player
-                    val targetLocation = directlyHitPlayer.location.clone().add(0.0, 0.5, 0.0)
-                    applyLaserEffects(player, directlyHitPlayer, targetLocation, level)
-                    vampireWithPerk(player)
-                    execution(player, directlyHitPlayer, level)
-                    laserCooldown[player.uniqueId] = Cooldown(15, TimeUnit.SECONDS)
-                    PitManager.givePlayerSpeedBuff(player, level * 2 * 20 + 40, level)
-                    return
-
+        val maxIterations = if (player.isSneaking) laserLength * 2 else laserLength
+        for (i in 0..maxIterations) {
+            val particleLocation = startLocation.clone().add(direction.clone().multiply(i * 0.5))
+            Bukkit.getOnlinePlayers()
+                .forEach { targetPlayer -> sendRedstoneParticle(targetPlayer, particleLocation, r, g, b) }
+            val nearbyEntities = particleLocation.world.getNearbyEntities(particleLocation, 0.5, 0.5, 0.5)
+            for (entity in nearbyEntities) {
+                if (entity !is Player || entity == player || entity.isDead || entity.location.y > 100) continue
+                entity.noDamageTicks = 0
+                val targetLoc = entity.location.clone().add(0.0, 0.5, 0.0)
+                applyLaserEffects(player, entity, targetLoc, level)
+                execution(player, entity, targetLoc, level)
+                vampireWithPerk(player)
+                if (player.isSneaking) {
+                    if (!Companion.blueLaserCooldown.getOrDefault(player.uniqueId, Cooldown(0)).hasExpired()) return
+                    player.teleport(entity.location.clone())
+                    blueLaserCooldown[player.uniqueId] = Cooldown(15, TimeUnit.SECONDS)
+                    PitManager.givePlayerSpeedBuff(player, level * 2 * 20 + 4 * 20, level)
+                    blueLaserCooldown[player.uniqueId] = Cooldown(15, TimeUnit.SECONDS)
                 }
-            }
-        } else {
-            for (i in 0..laserLength) {
-                val particleLocation = startLocation.clone().add(direction.clone().multiply(i * 0.5))
-                Bukkit.getOnlinePlayers()
-                    .forEach { targetPlayer -> sendRedstoneParticle(targetPlayer, particleLocation, 255f, 0f, 0f) }
-                val nearbyEntities = particleLocation.world.getNearbyEntities(particleLocation, 0.5, 0.5, 0.5)
-                for (entity in nearbyEntities) {
-                    if (entity !is Player || entity == player || entity.isDead || entity.location.y > 100) continue
-                    val directlyHitPlayer = entity
-                    val targetLocation = directlyHitPlayer.location.clone().add(0.0, 0.5, 0.0)
-                    applyLaserEffects(player, directlyHitPlayer, targetLocation, level)
-                    vampireWithPerk(player)
-                    execution(player, directlyHitPlayer, level)
-                    laserCooldown[player.uniqueId] = Cooldown(1, TimeUnit.SECONDS)
-                    return
-                }
+                return
             }
         }
-        laserCooldown[player.uniqueId] = Cooldown(1, TimeUnit.SECONDS)
+    }
+
+    override fun handleShootEntity(
+        enchantLevel: Int,
+        attacker: Player,
+        target: Entity,
+        damage: Double,
+        finalDamage: AtomicDouble?,
+        boostDamage: AtomicDouble?,
+        cancel: AtomicBoolean?
+    ) {
+        if (!Companion.redLaserCooldown.getOrDefault(attacker.uniqueId, Cooldown(0)).hasExpired()) return
+        if (target !is Player) return
+        val direction = target
+            .eyeLocation
+            .clone()
+            .add(0.0, 0.5, 0.0)
+            .toVector()
+            .subtract(attacker.eyeLocation.toVector()).normalize()
+        shootLaser(attacker, enchantLevel, attacker.eyeLocation, direction, 255f, 0f, 0f)
     }
 
     private fun vampireWithPerk(player: Player) {
@@ -159,16 +171,37 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
         }
     }
 
-    private fun execution(attacker: Player, target: Player, level: Int) {
-        val healthThreshold = level * 0.5 + 1.0
-        if (target.health <= healthThreshold * 2) {
-            PlayerUtil.damage(
-                attacker,
-                target,
-                PlayerUtil.DamageType.TRUE,
-                healthThreshold * 2,
-                false
-            )
+    private fun execution(attacker: Player, target: Player, location: Location, level: Int) {
+        val nearbyPlayers = location.world.getNearbyEntities(location, 4.0, 4.0, 4.0)
+        for (entity in nearbyPlayers) {
+            if (entity !is Player || entity == target || entity == attacker || entity.isDead) continue
+            val healthThreshold = level * 0.5 + 0.5
+            if (entity.health <= healthThreshold * 2) {
+                PlayerUtil.damage(
+                    attacker,
+                    entity,
+                    PlayerUtil.DamageType.TRUE,
+                    healthThreshold * 2,
+                    false
+                )
+                attacker.playSound(attacker.location, Sound.VILLAGER_DEATH, 1f, 0.5f)
+                val deathLoc = entity.location
+                val packetA = PacketPlayOutWorldEvent(
+                    2001,
+                    BlockPosition(deathLoc.blockX, deathLoc.blockY, deathLoc.blockZ),
+                    152,
+                    false
+                )
+                val packetB = PacketPlayOutWorldEvent(
+                    2001,
+                    BlockPosition(deathLoc.blockX, deathLoc.blockY - 1, deathLoc.blockZ),
+                    152,
+                    false
+                )
+                val connection = (attacker as CraftPlayer).handle.playerConnection
+                connection.sendPacket(packetA)
+                connection.sendPacket(packetB)
+            }
         }
     }
 
@@ -177,10 +210,6 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
         target.damage(maxDamage)
         location.world.playSound(location, Sound.EXPLODE, 1.0f, 1.0f)
         location.world.playEffect(location, Effect.EXPLOSION_HUGE, null)
-        if (attacker.isSneaking) {
-            val attackerTeleportLocation = location.clone().add(0.0, 0.5, 0.0)
-            attacker.teleport(attackerTeleportLocation)
-        }
         val nearbyPlayers = location.world.getNearbyEntities(location, 4.0, 4.0, 4.0)
         for (entity in nearbyPlayers) {
             if (entity !is Player || entity == target || entity == attacker || entity.isDead) continue
@@ -196,16 +225,9 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
 
     private fun applyEffect(target: Player, level: Int) {
         if (target.hasPotionEffect(PotionEffectType.SLOW)) target.removePotionEffect(PotionEffectType.SLOW)
-        if (target.hasPotionEffect(PotionEffectType.WITHER)) target.removePotionEffect(PotionEffectType.WITHER)
-
-        val buffLevel = when (level) {
-            1 -> 0 // 缓慢 I
-            2 -> 0 // 缓慢 I
-            3 -> 1 // 缓慢 II
-            else -> level - 2
-        }
-        target.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 20 * level, buffLevel))
-        target.addPotionEffect(PotionEffect(PotionEffectType.WITHER, 20 * level, buffLevel))
+        if (target.hasPotionEffect(PotionEffectType.WEAKNESS)) target.removePotionEffect(PotionEffectType.WEAKNESS)
+        target.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 20 * level + 20 * 2, 0))
+        target.addPotionEffect(PotionEffect(PotionEffectType.WEAKNESS, 20 * level + 20 * 2, level))
     }
 
     private fun setVector(
@@ -220,7 +242,7 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
         entity.velocity = vector
     }
 
-    private fun sendRedstoneParticle(sender: Player, location: org.bukkit.Location, r: Float, g: Float, b: Float) {
+    private fun sendRedstoneParticle(sender: Player, location: Location, r: Float, g: Float, b: Float) {
         val packet = net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles(
             net.minecraft.server.v1_8_R3.EnumParticle.REDSTONE,
             true,
@@ -239,24 +261,27 @@ class DesertRose : AbstractEnchantment(), IPlayerShootEntity, Listener, IActionD
     @EventHandler
     fun onQuit(e: PlayerQuitEvent) {
         Companion.cooldown.remove(e.player.uniqueId)
+        Companion.redLaserCooldown.remove(e.player.uniqueId)
+        Companion.blueLaserCooldown.remove(e.player.uniqueId)
     }
-
-    override fun handleShootEntity(
-        enchantLevel: Int,
-        attacker: Player,
-        target: Entity,
-        damage: Double,
-        finalDamage: AtomicDouble?,
-        boostDamage: AtomicDouble?,
-        cancel: AtomicBoolean?
-    ) {
-        if (!Companion.laserCooldown.getOrDefault(attacker.uniqueId, Cooldown(0)).hasExpired()) return
-        if (target !is Player) return
-        shootLaser(attacker, enchantLevel)
-    }
-
 
     override fun getText(level: Int, player: Player): String {
-        return getCooldownActionText(Companion.laserCooldown.getOrDefault(player.uniqueId, Cooldown(0L)))
+        return this.getCooldownActionText(
+            Companion.redLaserCooldown.getOrDefault(
+                player.uniqueId,
+                Cooldown(0L)
+            )
+        ) + " &7| " + this.getCooldownActionText(
+            Companion.blueLaserCooldown.getOrDefault(
+                player.uniqueId,
+                Cooldown(0L)
+            )
+        )
+    }
+
+    override fun getCooldownActionText(cooldown: Cooldown): String {
+        return if (cooldown.hasExpired()) "&a&l✔" else "&c&l" + net.mizukilab.pit.util.time.TimeUtil.millisToRoundedTime(
+            cooldown.remaining
+        ).replace(" ", "")
     }
 }
